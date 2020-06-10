3f84c51aec6d3fb1c14801d1820b46b6b1004902
tommydear <66705829+tommydear@users.noreply.github.com>
2020-06-10 14:58:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-python bot.py +[phone1] doge
-python bot.py +[phone1] zec
-python bot.py +[phone1] ltc
-python bot.py +[phone1] btc
-python bot.py +[phone1] bch
+python bot.py +2348100289529 doge
+python bot.py +2348100289529 zec
+python bot.py +2348100289529 ltc
+python bot.py +2348100289529 btc
+python bot.py +2348100289529 bch
 clear
 pwd
 bash start.sh
